17c5bf03ee13ab050cbc346c938a8fc24b66789d
RobertBlackwell-05 <robertblackwell@westernwyoming.edu>
2025-01-27 05:29:14 +0000

    Ready for grading.

--- hello-world/main.cpp
+++ hello-world/main.cpp
@@ -1,7 +1,16 @@
+
 //
-// Name
-// Date
-// Hello World Programming Project
+// Name Robert Blackwell
+// Date 1/26/2025
+// Your First C++ Program
 // COSC 1030
 //
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!";
+    return 0;
+}
 
