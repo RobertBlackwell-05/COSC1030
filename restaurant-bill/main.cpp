3d6c2e2826d333899688d2744758a2ccd80bed90
RobertBlackwell-05 <robertblackwell@westernwyoming.edu>
2025-02-04 23:09:43 +0000

    Ready For Grading.

--- restaurant-bill/main.cpp
+++ restaurant-bill/main.cpp
@@ -1,10 +1,27 @@
 //
-// Name
-// Date
+// Name Robert Blackwell
+// Date 2/4/2025 (2days within due date)
 // Restaurant Bill Programming Project
 // COSC 1030
 //
+#include <iostream>
+using namespace std;
 
+int main ()
+{
+    double meal = 44.50;
+    double tax = meal * 0.0675;
+    double subtotal = meal + tax;
+    double tip = subtotal * 0.20;
+    double total = subtotal + tip;
+
+    cout  << "Meal cost: " << meal << endl;
+    cout << "Tax: " << tax << endl;
+    cout << "Tip: " << tip << endl;
+    cout << "Total: " << total << endl;
+
+    return 0;
+}
 // Variable declarations
 
 // Constants for the tax rate and tip percentage
