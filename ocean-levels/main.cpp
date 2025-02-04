22ee00edbac8c84aec97047a4795c3fbc4e34e45
RobertBlackwell-05 <robertblackwell@westernwyoming.edu>
2025-02-04 23:38:31 +0000

    Ready For Grading

--- ocean-levels/main.cpp
+++ ocean-levels/main.cpp
@@ -1,8 +1,23 @@
 //
-// Name
-// Date
+// Name Robert Blackwell
+// Date 2/4/2025
 // Ocean Levels Programming Project
 // COSC 1030
 //
+#include <iostream>
+using namespace std;
 
+int main ()
+{
+    double oceanlevel = 2000;
+    double fiveyear = 1.5*5;
+    double sevenyear = 1.5*7;
+    double tenyear = 1.5*10;
 
+    cout << "The Ocean level currently is at: " << oceanlevel << endl;
+    cout << "The Ocean level has increased by: " << fiveyear << " milliters after five years" << endl;
+    cout << "The Ocean level has increased by: " << sevenyear << " milliters after seven years" << endl;
+    cout << "The Ocean level has increased by: " << tenyear << " milliters after ten years" << endl;
+
+    return 0;
+}
