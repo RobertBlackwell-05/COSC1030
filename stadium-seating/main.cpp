edaecd4bbc19071bfdf5f75a5e70ca3e479c58cb
RobertBlackwell-05 <robertblackwell@westernwyoming.edu>
2025-02-11 22:58:14 +0000

    Ready For grading

--- stadium-seating/main.cpp
+++ stadium-seating/main.cpp
@@ -1,7 +1,40 @@
 //
-// Name
-// Date
+// Name Robert Blackwell
+// Date 2/11/2025
 // Stadium Seating Programming Project
 // COSC 1030
 //
+#include <iostream>
+#include <iomanip>
+using namespace std;
 
+int main() 
+{
+    const double CLASS_A_PRICE = 15.0;
+    const double CLASS_B_PRICE = 12.0;
+    const double CLASS_C_PRICE = 9.0;
+
+    int classATickets;
+    int classBTickets;
+    int classCTickets;
+    double total;
+
+    cout << "Enter the number of Class A tickets sold: ";
+    cin >> classATickets;
+
+    cout << "Enter the number of Class B tickets sold: ";
+    cin >> classBTickets;
+
+    cout << "Enter the number of Class C tickets sold: ";
+    cin >> classCTickets;
+
+
+    total = (classATickets * CLASS_A_PRICE) +
+            (classBTickets * CLASS_B_PRICE) +
+            (classCTickets * CLASS_C_PRICE);
+
+cout << "The Total revenue from ticket sales is $"
+    << setprecision(2) << fixed << total << endl;
+
+return 0;
+}
