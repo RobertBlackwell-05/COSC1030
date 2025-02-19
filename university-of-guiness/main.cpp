828cdce1d0e66c08d5352bb73ac08f48842135c7
RobertBlackwell-05 <robertblackwell@westernwyoming.edu>
2025-02-19 05:31:07 +0000

    Ready for grading. Need help with this one, i can't figure out where and what I have done wrong.

--- university-of-guiness/main.cpp
+++ university-of-guiness/main.cpp
@@ -1,7 +1,58 @@
 //
-// Name
-// Date
+// Name Robert Blackwell
+// Date 2/18/2025
 // University of Guiness Programming Project
 // COSC 1030
 //
 
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    const double instatetution = 3000.0;
+    const double outstatetution = 4500.0;
+    const double ISroomandboard = 2500.0;
+    const double OSroomandboard =  3500.0;
+
+    int residency;
+    int roomandboard;
+    int yes;
+    int no;
+    int instate;
+    int outofstate;
+
+
+    cout << "Do you need room and board: " << endl;
+    cin >> roomandboard;
+
+    cout << "\nAre you in-state or out-of-state(One word): " << endl;
+    cin >> residency;
+
+    
+
+    if (residency = instate)
+    {
+        cout << "\nYour tution fee will be: $" << instatetution << endl;
+
+        if (roomandboard = yes)
+            {
+           cout << "\nYour room and board will be: $" << ISroomandboard << endl;
+            } 
+            else;
+    }
+
+    if (residency = outofstate)
+    {
+        cout << "\nYour tution fee will be: $" << outstatetution << endl;
+       
+             if (roomandboard = yes)
+            {
+            cout << "\nYour room and board will be: $" << OSroomandboard << endl;
+            }
+            else;
+    }
+
+    return 0;
+}
