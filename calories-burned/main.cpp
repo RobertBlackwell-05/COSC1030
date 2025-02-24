341058d94e3fbe9873537784d64e8ef63d66a94c
RobertBlackwell-05 <robertblackwell@westernwyoming.edu>
2025-02-24 06:56:39 +0000

    Ready for grading

--- calories-burned/main.cpp
+++ calories-burned/main.cpp
@@ -1,7 +1,23 @@
 //
-// Name
-// Date
+// Name Robert Blackwell
+// Date 2/23/25
 // Calories Burned Programming Project
 // COSC 1030
 //
 
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    const double CALS_PER_MINUTE = 3.9;
+    int minutes;
+    double calories;
+
+    for (minutes = 10; minutes <= 30; minutes += 5)
+    {
+        calories = minutes * CALS_PER_MINUTE;
+        cout << "After " << minutes << " minutes you burned " << calories << " calories.\n";
+    }
+    return 0;
+}
