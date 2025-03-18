319a3f76fa5c11b822956e1316548f2f846d216a
RobertBlackwell-05 <robertblackwell@westernwyoming.edu>
2025-03-18 22:23:06 +0000

    Ready for grading

--- markup/main.cpp
+++ markup/main.cpp
@@ -1,7 +1,50 @@
 //
-// Name
-// Date
+// Name Robert Blackwell
+// Date 3/18/2025
 // Markup Programming Project
 // COSC 1030
 //
+#include <iostream>
+#include <iomanip>
+using namespace std;
 
+double calculateRetail(double, double);
+
+int main ()
+{
+    double wholesale;
+    double markupPercent;
+    double retail; 
+
+    cout << "Enter the item's wholesale cost: ";
+    cin >> wholesale;
+
+    while (wholesale < 0)
+    {
+        cout << "Enter a non-negative value: ";
+        cin >> wholesale;
+    }
+
+    cout << "Enter the item's markup percentage: ";
+    cin >> markupPercent;
+
+    while (markupPercent < 0)
+    {
+        cout << "Enter a non-negative value: ";
+        cin >> markupPercent; 
+    }
+
+    retail = calculateRetail(wholesale, markupPercent);
+
+    cout << "The retail price is $" << setprecision(2)
+        << fixed << retail << endl;
+
+
+return 0;
+}
+
+double calculateRetail(double wholesale, double markupPct)
+{
+    double markup = wholesale * markupPct / 100;
+    return wholesale + markup;
+}
