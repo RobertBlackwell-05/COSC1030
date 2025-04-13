39ffdd1bc41c269ad8a7b9f96944ce275e196f66
RobertBlackwell-05 <robertblackwell@westernwyoming.edu>
2025-04-13 11:08:00 +0000

    Ready For Grading

--- pointer-rewrite/main.cpp
+++ pointer-rewrite/main.cpp
@@ -1,7 +1,37 @@
 //
-// Name
-// Date
+// Name Robert Blackwell
+// Date 4/13/2025
 // Pointer Rewrite Programming Project
 // COSC 1030
 //
+#include <iostream>
+using namespace std;
 
+int doSomthing(int *, int *);
+
+int main()
+{
+    int x = 10, y = 2;
+
+    cout << "Before calling the function:\n"
+        << "x = " << x << endl
+        << "y = " << endl << endl;
+    
+    cout << "The function returns "
+         << doSomthing(&x, &y) << endl << endl;
+    
+    cout << "After calling the function:\n"
+         << "x = " << x << endl
+         << "y = " << y << endl;
+
+return 0;
+
+}
+
+int doSomthing(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y * 10;
+    *y = temp * 10;
+    return *x + *y;
+}
