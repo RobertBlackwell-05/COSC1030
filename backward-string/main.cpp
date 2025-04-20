a0b647cc1b2bb4adec74316a7679098284a34c52
RobertBlackwell-05 <robertblackwell@westernwyoming.edu>
2025-04-20 06:18:35 +0000

    Ready For Grading

--- backward-string/main.cpp
+++ backward-string/main.cpp
@@ -1,7 +1,34 @@
 //
-// Name
-// Date
+// Name Robert Blackwell
+// Date 4/19/2025
 // Backward String Programming Project
 // COSC 1030
 //
+#include <iostream>
+#include <cstring>
+using namespace std;
 
+
+void backward(char *);
+
+int main()
+{
+    const int SIZE = 80;
+    char input[SIZE];
+
+    cout << "Enter a string and I will display it backward: \n";
+    cin.getline(input, SIZE);
+
+    backward(input);
+    return 0;
+}
+
+void backward(char *str)
+{
+    int last = strlen(str) -1;
+
+    for (int index = last; index >= 0; index--)
+        cout << str[index];
+
+    cout << endl;
+}
