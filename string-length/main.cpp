372d0c3d7d5bf31d279205bc5fd00c2db1fa11a8
RobertBlackwell-05 <robertblackwell@westernwyoming.edu>
2025-05-05 07:10:45 +0000

    ready for grading

--- string-length/main.cpp
+++ string-length/main.cpp
@@ -1,7 +1,30 @@
 //
-// Name
-// Date
+// Name Robert Blackwell
+// Date 5/5/2025
 // String Length Programming Project
 // COSC 1030
 //
+#include <iostream>
 
+int stringLength(const char* str)
+{
+    int count = 0;
+    while (*str != '\0')
+        {
+        count++;
+        str++;
+        }
+    return count;
+}
+
+int main()
+{
+    char inputString[100];
+    std::cout << "Enter a String: ";
+    std::cin.getline(inputString, 100);
+
+    int length = stringLength(inputString);
+    std::cout << "The length of the string is: " << length << std::endl;
+    return 0;
+
+}
